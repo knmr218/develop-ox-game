535ff62f21938122cfd4096ffb14810e3ac8fc5b
knmr218 <134046787+knmr218@users.noreply.github.com>
2024-10-26 04:19:16 +0000

    pusher,laravel-echoの導入

--- ox-game/vite.config.js
+++ ox-game/vite.config.js
@@ -8,4 +8,19 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-cert.pem')),
+        },
+        hmr: {
+            host: '172.18.0.3',
+            port: 5173,
+            protocol: 'wss', // WebSocket over SSL
+        },
+    },
+    envPrefix: 'VITE_',
 });
